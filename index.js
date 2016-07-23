bbe25b796cecf502b1e868c96008ceb0a67e69f2
Lex Robinson <lexi@lexi.org.uk>
2016-07-23 16:26:19 +0100

    Sessions!
    (I don't know what for)

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressWinston = require('express-winston');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
 
 const log = require('./src/logging');
 const { tokeGET, tokePOST } = require('./src/routes');
 
+const dbPromise = mongoose.connect(
+	'mongodb://localhost/mean-dev',
+	{
+		user: '',
+		pass: '',
+	}
+);
 const app = express();
 
 app.use(expressWinston.logger({
@@ -20,19 +29,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/toke', tokeGET);
 app.post('/toke', tokePOST);
 
+app.use(session({
+	secret: '420 BlAzE IT!!!',
+	store: new MongoStore({ dbPromise }),
+}));
+
 const httpPort = 8080;
 
 // Fucking dumbass shit
 mongoose.Promise = Promise;
 
-mongoose.connect(
-	'mongodb://localhost/mean-dev',
-	{
-		user: '',
-		pass: '',
-	}
-)
-.then(
+
+dbPromise.then(
 	() => {
 		log.info("Connected to MongoDB");
 		app.listen(httpPort, () => {
