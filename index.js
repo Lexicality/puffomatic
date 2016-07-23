d1d3af1c82a5826b7ba1fd3e78449ff48d331efe
Lex Robinson <lexi@lexi.org.uk>
2016-07-23 14:12:38 +0100

    Fuck

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.bodyParser())
+app.use(bodyParser.json());
 
 app.get('/toke', (req, res) => {
 	res.send("Nice");
