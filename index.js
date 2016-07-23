144a26f9952cc3f2751cb7d81033e9067c58adb5
Lex Robinson <lexi@lexi.org.uk>
2016-07-23 14:09:18 +0100

    Add an API endpoint

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.bodyParser())
+
 app.get('/toke', (req, res) => {
 	res.send("Nice");
+});
+
+app.post('/toke', (req, res) => {
+	console.log(req.body);
+	res.send({response: "Nice"});
 });
 
 app.listen(8080);
